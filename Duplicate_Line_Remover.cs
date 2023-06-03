b07343f598fdf90125561b6e21cd723341a9cdfb
Kooreis <kooreis.official@gmail.com>
2023-06-03 17:38:09 -0500

    In this update, the initial setup of the program is done. It checks if the correct number of arguments are provided. If not, it prints a usage message and exits.

--- Duplicate_Line_Remover.cs
+++ Duplicate_Line_Remover.cs
@@ -1,7 +1,5 @@
-```CSharp
 using System;
 using System.IO;
-using System.Linq;
 
 class Program
 {
@@ -12,22 +10,5 @@ class Program
             Console.WriteLine("Usage: DuplicateLineRemover <inputfile> <outputfile>");
             return;
         }
-
-        string inputFilePath = args[0];
-        string outputFilePath = args[1];
-
-        if (!File.Exists(inputFilePath))
-        {
-            Console.WriteLine("Input file does not exist.");
-            return;
-        }
-
-        var lines = File.ReadAllLines(inputFilePath);
-        var distinctLines = lines.Distinct();
-
-        File.WriteAllLines(outputFilePath, distinctLines);
-
-        Console.WriteLine("Duplicate lines removed successfully.");
     }
 }
-```
