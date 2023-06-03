516623d1e05abfcb8359c0e93f0c3d5f6e90d8af
Kooreis <kooreis.official@gmail.com>
2023-06-03 17:38:09 -0500

    This part of the script defines a function called 'remove_duplicates' that takes a filename as an argument. It then opens the file in read mode and reads all the lines.

--- Duplicate_Line_Remover.py
+++ Duplicate_Line_Remover.py
@@ -1,8 +1,2 @@
 def remove_duplicates(filename):
     lines = open(filename, 'r').readlines()
-    lines_set = set(lines)
-    out = open(filename, 'w')
-    for line in lines_set:
-        out.write(line)
-
-remove_duplicates('yourfile.txt')
