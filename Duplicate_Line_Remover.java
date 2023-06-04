999068bd37e1adeea4453da2fb2ccfc17d5fa8d9
Kooreis <kooreis.official@gmail.com>
2023-06-04 17:38:09 -0500

    Created the main class and method, and initialized the BufferedReader to read from the input file. Also, initialized a Set to store the unique lines from the file.

--- Duplicate_Line_Remover.java
+++ Duplicate_Line_Remover.java
@@ -1,2 +1,5 @@
-import java.io.*;
-import java.util.*;
+public class DuplicateLineRemover {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        Set<String> lines = new LinkedHashSet<String>();
+        String line;
